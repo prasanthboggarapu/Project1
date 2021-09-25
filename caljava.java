c48c46f052bf44d7896eec930b7aa4bdd7d972cc
prasanthboggarapu <prasanth.boggarapu@gmail.com>
2021-09-25 22:59:37 +0530

    Master chnaged

--- caljava.java
+++ caljava.java
@@ -1,32 +1,24 @@
-<<<<<<< HEAD
-/adding this to new branch1 /
-
-=======
-/from branch2/
->>>>>>> branch2
 import java.util.Scanner;
 
-public class calculator{
+public class  caljava{
 
-	public static void main(Strig args[]){
-	float a, b
+	public static void main(String args[]){
+	float a, b;
+	char choice;
 	Scanner scan = new Scanner(System.in);
-
+	
 	do{
-		System.out.println("Enter The two values:" );
-		a = scan.nextfloat();
-		b = scan.nextfloat();
-		res = a + b;
 		choice = scan.next().charAt(0);
-		switch(choice){
-			case "1":
+		switch(choice)
+		{
+			case '1':
 				System.out.println("Enter The two values:" );
-                		a = scan.nextfloat();
-                		b = scan.nextfloat();
-				res = a + b;
+                		a = scan.nextFloat();
+                		b = scan.nextFloat();
+				Float res = a + b;
 				System.out.println("Result:"+"\t" + res);
 				break;
-			case "2":
+			case '2':
 				System.exit(0);
 			default:
 				System.out.println("Invalid choice");
@@ -36,7 +28,7 @@ public class calculator{
 		}
 
 		System.out.println("\n---------------------------------\n");
-		}while(choice != 2);
+	}while(choice != 2);
 	}
 
 }
