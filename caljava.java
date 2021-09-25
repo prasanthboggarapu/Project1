6ef08affc3f47ab74a429b8c25b9391363e729f3
prasanthboggarapu <prasanth.boggarapu@gmail.com>
2021-09-25 23:08:31 +0530

    Added case 4 from branch2

--- caljava.java
+++ caljava.java
@@ -1,32 +1,33 @@
-/adding this from branch2
-/adding this to new branch1 /
-
-=======
-/from branch2/
->>>>>>> branch2
 import java.util.Scanner;
 
 public class calculator{
 
 	public static void main(Strig args[]){
-	float a, b
+	float a, b;
+	char choice;
 	Scanner scan = new Scanner(System.in);
 
 	do{
-		System.out.println("Enter The two values:" );
+		System.out.println("Enter The a value:" );
 		a = scan.nextfloat();
+		 System.out.println("Enter The b value:" );
 		b = scan.nextfloat();
-		res = a + b;
+		Float res;
+		System.out.println("Enter 1 or 2 or 3 or 4:");
 		choice = scan.next().charAt(0);
 		switch(choice){
-			case "1":
-				System.out.println("Enter The two values:" );
-                		a = scan.nextfloat();
-                		b = scan.nextfloat();
+			case '1':
 				res = a + b;
-				System.out.println("Result:"+"\t" + res);
+				System.out.println("Result of addition:"+"\t" + res);
 				break;
-			case "2":
+			case '2':
+				 res = a - b;
+                                System.out.println("Result of Subtraction:"+"\t" + res);
+				break;
+			case '3':
+				res = a * b'
+                                System.out.println("Result of Subtraction:"+"\t" + res);
+			case '4':
 				System.exit(0);
 			default:
 				System.out.println("Invalid choice");
