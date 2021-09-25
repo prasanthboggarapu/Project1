de6ae56e7fd1740c006cd0babc31ecdc4eb206a8
prasanthboggarapu <prasanth.boggarapu@gmail.com>
2021-09-25 21:19:50 +0530

    COmmit to master branch

---
+++ caljava.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class calculator{
+
+	public static void main(Strig args[]){
+	float a, b
+	Scanner scan = new Scanner(System.in);
+
+	do{
+		System.out.println("Enter The two values:" );
+		a = scan.nextfloat();
+		b = scan.nextfloat();
+		res = a + b;
+		choice = scan.next().charAt(0);
+		switch(choice){
+			case "1":
+				System.out.println("Enter The two values:" );
+                		a = scan.nextfloat();
+                		b = scan.nextfloat();
+				res = a + b;
+				System.out.println("Result:"+"\t" + res);
+				break;
+			case "2":
+				System.exit(0);
+			default:
+				System.out.println("Invalid choice");
+				break;
+
+
+		}
+
+		System.out.println("\n---------------------------------\n");
+		}while(choice != 2);
+	}
+
+}
