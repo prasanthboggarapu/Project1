3a504c3015cf5e8f6fb1e7c59172f1919f8876be
prashanthBoggavarapu <91388819+prashanthBoggavarapu@users.noreply.github.com>
2021-09-25 23:58:16 +0530

    Update caljava.java
    Added division

--- caljava.java
+++ caljava.java
@@ -13,7 +13,7 @@ public class caljava{
 		 	System.out.println("Enter The b value:" );
 			b = scan.nextFloat();
 			Float res;
-			System.out.println("Enter 1 or 2 or 3 or 4:");
+			System.out.println("Enter 1 or 2 or 3 or 4 or 5:");
 			choice = scan.next().charAt(0);
 		switch(choice){
 			case '1':
@@ -28,6 +28,9 @@ public class caljava{
 				res = a * b;
                                 System.out.println("Result of Subtraction:"+"\t" + res);
 			case '4':
+				res = a/b;
+				System.out.println("Result of Divide:"+"\t" + res);
+			case '5':
 				System.exit(0);
 			default :
 				System.out.println("Invalid choice");
